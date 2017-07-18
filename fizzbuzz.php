12dd7ce00028befb2292632fa1fda2c8abc7662b
Luong Viet Hai <viethaihn1994@gmail.com>
2017-07-18 15:34:52 +0700

    remove all file

--- fizzbuzz.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: windd01
- * Date: 07/07/2017
- * Time: 14:07
- */
--- output.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: windd01
- * Date: 18/07/2017
- * Time: 13:21
- */
